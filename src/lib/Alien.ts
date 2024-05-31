51765f00f9632720f12ea828fbdda8bf424467c3
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-05-31 16:56:26 -0300

    add Alien horizontal movement logic

--- src/lib/Alien.ts
+++ src/lib/Alien.ts
@@ -1,27 +1,46 @@
 import { v4 } from "uuid";
 import { GameObject, GameObjectConfig } from "./interfaces/GameObject";
+import { EngineState } from "./interfaces/Engine";
+
+export type AlienMotion = 1 | -1;
 
 export interface AlienConfig extends GameObjectConfig {
   pointsWorth: number;
+  motion: AlienMotion;
 }
 
 export class Alien implements GameObject {
   private readonly id = v4();
   private readonly canBeDestroyed = true;
+  private currentMotion: AlienMotion = 1;
 
   constructor(
     private readonly config: Omit<GameObjectConfig, "id" | "canBeDestroyed">,
     private readonly pointsWorth: number
-  ) {}
+  ) {
+    config.velocityX < 0 && (this.currentMotion = -1);
+  }
 
   getData(): AlienConfig {
     return {
       id: this.id,
       canBeDestroyed: this.canBeDestroyed,
       pointsWorth: this.pointsWorth,
+      motion: this.currentMotion,
       ...this.config,
     };
   }
 
-  update(): void {}
+  update(engineState: EngineState): void {
+    const stores = engineState.stores;
+    const velocityMultiplier = stores["velocity-multiplier"] ?? 1;
+    const motion = stores["motion"] ?? this.currentMotion;
+    if (motion != this.currentMotion) {
+      this.currentMotion = -this.currentMotion as AlienMotion;
+      this.config.velocityX = -this.config.velocityX;
+      this.config.positionY += this.config.velocityY;
+    }
+
+    this.config.positionX += this.config.velocityX * velocityMultiplier;
+  }
 }
