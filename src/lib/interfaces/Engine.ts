343b2ffac3c7843c570badd8968fe1e9ff53cb84
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-04 17:00:13 -0300

    change Engine gameObject requirements to renderableObjects

--- src/lib/interfaces/Engine.ts
+++ src/lib/interfaces/Engine.ts
@@ -1,28 +1,27 @@
 import { CollisionSystem } from "./CollisionSystem";
-import { GameObject } from "./GameObject";
 import { InputSystemState } from "./InputSystem";
 import { LogicScript } from "./LogicScript";
 
 export type GameState = "running" | "not-running" | "triggered-to-stop";
 
 export interface EngineState {
-  gameObjects: Record<string, GameObject>;
+  renderableObjects: Record<string, any>;
   stores: Record<string, any>;
   gameState: GameState;
   collisionSystem: CollisionSystem;
   inputSystemState: InputSystemState;
   requestStoresEdit: (key: string, newValue: any, toDelete: boolean) => void;
   requestGameStop: () => void;
-  requestGameObjectAdd: (gameObject: GameObject) => void;
-  requestGameObjectDestruction: (id: string) => void;
+  requestRenderableObjectAdd: (RenderableObject: any) => void;
+  requestRenderableObjectDestruction: (id: string) => void;
 }
 
 export abstract class Engine {
   abstract startGameLoop(): void;
   abstract stopGameLoop(): void;
-  abstract addOneGameObject(newGameObject: GameObject): void;
-  abstract addManyGameObjects(newGameObjects: GameObject[]): void;
-  abstract destroyGameObject(id: string): void;
+  abstract addOneRenderableObject(newRenderableObject: any): void;
+  abstract addManyRenderableObjects(newRenderableObjects: any[]): void;
+  abstract destroyRenderableObject(id: string): void;
   abstract addLogicScript(script: LogicScript): void;
   abstract requestLogicScriptDestruction(id: any): void;
   abstract getEngineState(): EngineState;
