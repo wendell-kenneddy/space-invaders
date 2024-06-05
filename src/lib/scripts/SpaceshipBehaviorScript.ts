5ab72c4541a4ed9b8db3b9ebb860c80721753d1a
Wendell Kenneddy <wkenneddy505@gmail.com>
2024-06-05 14:21:53 -0300

    (SpaceshipBehaviorScript): update types to use InteractableObject

--- src/lib/scripts/SpaceshipBehaviorScript.ts
+++ src/lib/scripts/SpaceshipBehaviorScript.ts
@@ -2,7 +2,7 @@ import { v4 } from "uuid";
 import { LogicScript, LogicScriptData } from "@interfaces/LogicScript";
 import { EngineState } from "@interfaces/Engine";
 import { Projectile } from "@game-objects/Projectile";
-import { GameObjectConfig } from "@interfaces/GameObject";
+import { GameObjectConfig } from "@interfaces/InteractableObject";
 
 export class SpaceshipBehaviorScript implements LogicScript {
   private readonly id = v4();
